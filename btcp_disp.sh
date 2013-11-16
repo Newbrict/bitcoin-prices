9182b2f4ed5121626373fd9f0a285f636d334cb3
Dimitar Dimitrov <newbrict@gmail.com>
2013-11-15 21:39:29 -0500

    fixed the fix i just pretended to fix

--- btcp_disp.sh
+++ btcp_disp.sh
@@ -6,6 +6,9 @@
 [[ ! $(pgrep btcp.sh) ]] && (./btcp.sh &) &&
 	echo "Starting gatherer, rerun after 1 minute for real data" && exit 0
 
+# exit if there are no prices
+[[ ! -f "/tmp/btcp.txt" ]] && echo "no prices found" && exit 1
+
 # display prices
 prices=$(cat /tmp/btcp.txt)
 
@@ -19,7 +22,7 @@ function printer() {
 	diff="$(echo $2 | cut -d ":" -f 2)"
 
 	echo -ne "$1: "
-	if [ $diff = 0 ]
+	if [ "$diff" = "0" ]
 	then
 		echo -e "\e[40m\e[0;37m$price ($diff)\e[0m"
 	elif [ "$(echo $diff | grep -o "-")" == "-" ]
