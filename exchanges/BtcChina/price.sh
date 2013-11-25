a130e534bd3ba68c2597c71d79da2bac4a3363b8
Dimitar Dimitrov <newbrict@gmail.com>
2013-11-25 04:52:44 -0500

    added btcchina

---
+++ exchanges/BtcChina/price.sh
@@ -0,0 +1,11 @@
+base="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+out="$base/data/api.txt"
+outc="$base/data/conv.txt"
+[[ -f $out ]] &&
+	price="$(cat $out | cut -d ',' -f5  | cut -d ':' -f2 | perl -pe "s/\"//g")" &&
+	conv="$(cat $outc | cut -d ',' -f2  | cut -d ':' -f2)" &&
+	[[ $price = *[0-9]* && $price = ?([+-])*([0-9])?(.*([0-9])) ]] &&
+	[[ $conv = *[0-9]* && $price = ?([+-])*([0-9])?(.*([0-9])) ]] &&
+		price=$(bc <<< "$price*$conv") &&
+		echo $price | perl -pe "s/([0-9]*\....).*/\1/" ||
+		echo 0
