2f347d6d44868e166970eaf419d068e271f74af4
Dimitar Dimitrov <newbrict@gmail.com>
2013-11-27 13:05:06 -0500

    fixed comma issue

--- exchanges/BtcChina/price.sh
+++ exchanges/BtcChina/price.sh
@@ -2,7 +2,7 @@ base="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 out="$base/data/api.txt"
 outc="$base/data/conv.txt"
 [[ -f $out ]] &&
-	price="$(cat $out | cut -d ',' -f5  | cut -d ':' -f2 | perl -pe "s/\"//g")" &&
+	price="$(cat $out | cut -d ':' -f7  | cut -d ',' -f1 | perl -pe "s/\"//g")" &&
 	conv="$(cat $outc | cut -d ',' -f2  | cut -d ':' -f2)" &&
 	[[ $price = *[0-9]* && $price = ?([+-])*([0-9])?(.*([0-9])) ]] &&
 	[[ $conv = *[0-9]* && $price = ?([+-])*([0-9])?(.*([0-9])) ]] &&
