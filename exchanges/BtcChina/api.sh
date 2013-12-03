2f38d3d2a8935bd5ede19a4413a4c6e98e499192
Dimitar Dimitrov <newbrict@gmail.com>
2013-12-02 19:36:51 -0500

    btcchina can't make up their mind

--- exchanges/BtcChina/api.sh
+++ exchanges/BtcChina/api.sh
@@ -1,3 +1,3 @@
 base="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-curl -k -s "https://vip.btcchina.com/bc/ticker" > $base/data/api.txt
+curl -k -s "https://data.btcchina.com/data/ticker" > $base/data/api.txt
 curl -s "http://rate-exchange.appspot.com/currency?from=CNY&to=USD" > $base/data/conv.txt
