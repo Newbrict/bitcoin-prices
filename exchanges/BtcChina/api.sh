9befa3c9301d2f8455fe1261e5ccbd7f309e994e
Dimitar Dimitrov <newbrict@gmail.com>
2014-01-14 21:24:24 -0500

    added a comment

--- exchanges/BtcChina/api.sh
+++ exchanges/BtcChina/api.sh
@@ -1,3 +1,4 @@
 base="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 curl -k -s "https://data.btcchina.com/data/ticker" > $base/data/api.txt
+# now google puts quotes around their conversions
 curl -s "http://rate-exchange.appspot.com/currency?from=CNY&to=USD" > $base/data/conv.txt
