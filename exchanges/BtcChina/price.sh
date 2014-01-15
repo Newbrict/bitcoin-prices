ae54b7d87e2cc07341a0924a8d508804b87f9eb1
Dimitar Dimitrov <newbrict@gmail.com>
2014-01-14 21:21:52 -0500

    fixed google conversion bug

--- exchanges/BtcChina/price.sh
+++ exchanges/BtcChina/price.sh
@@ -3,9 +3,8 @@ out="$base/data/api.txt"
 outc="$base/data/conv.txt"
 [[ -f $out ]] &&
 	price="$(cat $out | cut -d ':' -f7  | cut -d ',' -f1 | perl -pe "s/\"//g")" &&
-	conv="$(cat $outc | cut -d ',' -f2  | cut -d ':' -f2)" &&
+	conv="$(cat $outc | cut -d ',' -f2  | cut -d ':' -f2 | perl -pe "s/\"//g")" &&
 	[[ $price = *[0-9]* && $price = ?([+-])*([0-9])?(.*([0-9])) ]] &&
 	[[ $conv = *[0-9]* && $price = ?([+-])*([0-9])?(.*([0-9])) ]] &&
-		price=$(bc <<< "$price*$conv") &&
-		echo $price ||
+		echo $(echo "$price*$conv" | bc ) ||
 		echo 0
